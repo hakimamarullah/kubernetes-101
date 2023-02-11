fd5bd6193425f400a18f6a696aba0d39c893a7b9
hakimamarullah <hakimamarullah@gmail.com>
2023-02-11 15:38:50 +0700

    update

---
+++ keycloak/get-details.sh
@@ -0,0 +1,6 @@
+KEYCLOAK_URL=https://keycloak.10.240.0.11.nip.io &&
+echo "" &&
+echo "Keycloak:                 $KEYCLOAK_URL" &&
+echo "Keycloak Admin Console:   $KEYCLOAK_URL/admin" &&
+echo "Keycloak Account Console: $KEYCLOAK_URL/realms/myrealm/account" &&
+echo ""
