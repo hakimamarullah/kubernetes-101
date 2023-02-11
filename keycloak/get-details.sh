d5b221c5a3e0a5029504ee179ca388bde423b265
hakimamarullah <hakimamarullah@gmail.com>
2023-02-11 15:45:02 +0700

    update

--- keycloak/get-details.sh
+++ keycloak/get-details.sh
@@ -1,4 +1,4 @@
-KEYCLOAK_URL=https://keycloak.34.29.51.255.nip.io &&
+KEYCLOAK_URL=http://34.29.51.255:$(kubectl get services/keycloak -o go-template='{{(index .spec.ports 0).nodePort}}') &&
 echo "" &&
 echo "Keycloak:                 $KEYCLOAK_URL" &&
 echo "Keycloak Admin Console:   $KEYCLOAK_URL/admin" &&
