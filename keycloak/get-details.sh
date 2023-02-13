a7219f23476e1db07cf498ba3df36e4d9b40ec75
hakimamarullah <hakimamarullah@gmail.com>
2023-02-13 17:47:24 +0700

    update

--- keycloak/get-details.sh
+++ keycloak/get-details.sh
@@ -1,4 +1,5 @@
-KEYCLOAK_URL=http://34.29.51.255:$(kubectl get services/keycloak -o go-template='{{(index .spec.ports 0).nodePort}}') &&
+EXTERNAL_IP = $(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip --no-progress-meter) &&
+KEYCLOAK_URL=http://$EXTERNAL_IP:$(kubectl get services/keycloak -o go-template='{{(index .spec.ports 0).nodePort}}') &&
 echo "" &&
 echo "Keycloak:                 $KEYCLOAK_URL" &&
 echo "Keycloak Admin Console:   $KEYCLOAK_URL/admin" &&
